819563f455f50240c09f8c43dc9091ea003a025c
Richard Edgar <freesurfer.rge@gmail.com>
2021-11-23 09:01:33 -0500

    Finish up neighbour tests

--- life/test_sparse_set_state.py
+++ life/test_sparse_set_state.py
@@ -27,5 +27,39 @@ class TestGetNeighbours:
         # Don't need any active cells
         target = SparseSetState({})
 
-        actual = target.get_neighbours(cell, 2, 2)
+        actual = target.get_neighbours(cell, 3, 3)
+        self.check_neighbours(actual, expected)
+
+    @pytest.mark.parametrize(
+        "cell, expected",
+        [
+            ((1, 0), {(0, 0), (0, 1), (1, 1), (2, 1), (2, 0)}),
+            ((0, 1), {(0, 0), (1, 0), (1, 1), (1, 2), (0, 2)}),
+            ((1, 2), {(0, 2), (0, 1), (1, 1), (2, 1), (2, 2)}),
+            ((2, 1), {(2, 0), (1, 0), (1, 1), (1, 2), (2, 2)}),
+        ],
+        ids=["(1,0)", "(0,1)", "(1,2)", "(2,1)"],
+    )
+    def test_get_neighbours_edge(self, cell, expected):
+        # Don't need any active cells
+        target = SparseSetState({})
+
+        actual = target.get_neighbours(cell, 3, 3)
+        self.check_neighbours(actual, expected)
+
+    def test_get_neighbours_centre(self):
+        # Don't need any active cells
+        target = SparseSetState({})
+
+        actual = target.get_neighbours((1, 1), 3, 3)
+        expected = {
+            (0, 0),
+            (0, 1),
+            (0, 2),
+            (1, 0),
+            (1, 2),
+            (2, 0),
+            (2, 1),
+            (2, 2),
+        }
         self.check_neighbours(actual, expected)
