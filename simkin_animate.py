89a33e4315db865cfa83d3352ec94e5db559762c
Richard Edgar <freesurfer.rge@gmail.com>
2021-11-25 16:17:13 -0500

    Figuring out an animated GIF

--- simkin_animate.py
+++ simkin_animate.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import Set, Tuple
 
 from life import (
     LifeBoard,
@@ -11,16 +11,21 @@ from life import (
 from PIL import Image, ImageDraw
 
 
-nx = 1024
-ny = 1024
-dx = 500
-dy = 500
+nx = 512
+ny = 512
+dx = 250
+dy = 250
 
 
-def write_image(cells: List[Tuple[int, int]], name: str):
-    image = Image.new("1", (nx, ny))
+def get_image(cells: Set[Tuple[int, int]]) -> Image:
+    image = Image.new("L", (nx, ny))
     for c in cells:
-        image.putpixel((c[0], c[1]), 1)
+        image.putpixel((c[0], c[1]), 255)
+    return image
+
+
+def write_image(cells: Set[Tuple[int, int]], name: str):
+    image = get_image(cells)
     image.save(name)
 
 
@@ -30,21 +35,22 @@ def main():
 
     print("Read initial file")
 
-    initial_pattern = [(c[0] + dx, c[1] + dy) for c in pattern_from_file]
+    initial_pattern = set([(c[0] + dx, c[1] + dy) for c in pattern_from_file])
 
-    name_pattern = "image_{0:04}.png"
-    iteration = 0
-
-    write_image(initial_pattern, name_pattern.format(iteration))
+    images = []
+    images.append(get_image(initial_pattern))
 
     rules = SparseSetRules()
     state = SparseSetState(initial_pattern)
     board = LifeBoard(state, rules, nx, ny, x_wrap=True, y_wrap=True)
 
-    for _ in range(500):
+    for _ in range(1000):
         board.update()
-        iteration = iteration + 1
-        write_image(board.state.grid, name_pattern.format(iteration))
+        images.append(get_image(board.state.grid))
+
+    images[0].save(
+        "cells.gif", save_all=True, append_images=images[1:], duration=40
+    )
 
 
 main()
